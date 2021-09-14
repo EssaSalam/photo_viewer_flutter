5b1a54eaeddd851df09739bb2fd4124e4095efb6
EssaShehab <essa323371@gmail.com>
2021-09-14 05:56:19 +0300

    new improve

--- lib/main.dart
+++ lib/main.dart
@@ -18,42 +18,42 @@ class Image {
 List<Image> images = [
   Image(
       remoteUrl:
-          'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
+      'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg',
       localUrl: 'assets/imsages/a.png'),
   Image(
       remoteUrl:
-          'https://docs.microsoft.com/en-us/windows/apps/design/controls/images/image-licorice.jpg',
+      'https://docs.microsoft.com/en-us/windows/apps/design/controls/images/image-licorice.jpg',
       localUrl: 'assets/images/b.png'),
   Image(
       remoteUrl:
-          'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+      'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
       localUrl: 'assets/images/c.png'),
   Image(
       remoteUrl: 'https://cdn.wallpapersafari.com/78/8/nRNy24.jpg',
       localUrl: 'assets/images/d.png'),
   Image(
       remoteUrl:
-          'https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg',
+      'https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg',
       localUrl: 'assets/images/e.png'),
   Image(
       remoteUrl:
-          'https://www.inpixio.com/remove-background/images/main-before.jpg',
+      'https://www.inpixio.com/remove-background/images/main-before.jpg',
       localUrl: 'assets/images/f.png'),
   Image(
       remoteUrl:
-          'https://www.inpixio.com/remove-background/images/main-before.jpg',
+      'https://www.inpixio.com/remove-background/images/main-before.jpg',
       localUrl: 'assets/images/g.png'),
   Image(
       remoteUrl:
-          'https://www.inpixio.com/remove-background/images/main-before.jpg',
+      'https://www.inpixio.com/remove-background/images/main-before.jpg',
       localUrl: 'assets/images/h.png'),
   Image(
       remoteUrl:
-          'https://www.inpixio.com/remove-background/images/main-before.jpg',
+      'https://www.inpixio.com/remove-background/images/main-before.jpg',
       localUrl: 'assets/images/i.png'),
   Image(
       remoteUrl:
-          'https://www.inpixio.com/remove-background/images/main-before.jpg',
+      'https://www.inpixio.com/remove-background/images/main-before.jpg',
       localUrl: 'assets/images/l.png'),
 ];
 
@@ -71,34 +71,30 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text(""),
         ),
-        body: Center(
-          child: CarouselSlider(
-            items: images
-                .map((e) => Container(
-                    height: 200,
-                    width: 400,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Dismissible(
-                        direction: DismissDirection.down,
-                        key: Key(e.localUrl.toString()),
-                        onDismissed: (DismissDirection dir) => setState(() {
-                          images.removeAt(images.indexOf(e));
-                        }),
-                        child: Container(
-                          width: double.infinity,
-                          margin: EdgeInsets.symmetric(horizontal: 8),
-                          child: Card(
-                            child: DefaultBoxImage(element: e),
-                          ),
-                        ),
-                      ),
-                    )))
-                .toList(),
-            options: CarouselOptions(
-              height: 200,
-              enlargeCenterPage: true,
-              initialPage: 0,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: CarouselSlider(
+                items: images
+                    .map((e) => Dismissible(
+                  direction: DismissDirection.down,
+                  key: Key(e.localUrl.toString()),
+                  onDismissed: (DismissDirection dir) => setState(() {
+                    images.removeAt(images.indexOf(e));
+                  }),
+                  child: Card(
+                    child: DefaultBoxImage(element: e),
+                  ),
+                ))
+                    .toList(),
+                options: CarouselOptions(
+                  height: 220,
+                  // enlargeCenterPage: true,
+                  initialPage: 0,
+                ),
+              ),
             ),
           ),
         ),
@@ -140,62 +136,54 @@ class _DefaultBoxImageState extends State<DefaultBoxImage> {
             ),
           ),
         if (widget.element.remoteUrl.isNotEmpty)
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Builder(
-                builder: (ctx) => InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            ctx,
-                            MaterialPageRoute(
-                                builder: (_) => DefaultInteractiveImage(
-                                      images: images,
-                                      currentIndex:
-                                          images.indexOf(widget.element),
-                                    )));
-                      },
-                      child: Builder(builder: (context) {
-                        return Hero(
-                          tag: images.indexOf(widget.element),
-                          child: PhotoView(
-                            disableGestures: true,
-                            imageProvider: AssetImage(widget.element.localUrl),
-                            initialScale: PhotoViewComputedScale.covered,
-                            errorBuilder: (context, error, stackTrace) =>
-                                Center(
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  images[images.indexOf(widget.element)]
-                                      .localUrl = 'assets/images/a.png';
-                                  setState(() {});
-                                },
-                                style: ElevatedButton.styleFrom(
-                                    shape: CircleBorder(),
-                                    primary: Colors.white.withOpacity(0.5),
-                                    minimumSize: Size(50, 50)),
-                                child: const Icon(
-                                  Icons.refresh,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                            loadingBuilder: (context, event) => Center(
-                              child: SizedBox(
-                                width: 20.0,
-                                height: 20.0,
-                                child: CircularProgressIndicator(
-                                  value: event == null
-                                      ? 0
-                                      : event.cumulativeBytesLoaded /
-                                          event.expectedTotalBytes!,
-                                ),
-                              ),
-                            ),
+          Builder(
+              builder: (ctx) => InkWell(
+                onTap: () {
+                  Navigator.push(
+                      ctx,
+                      MaterialPageRoute(
+                          builder: (_) => DefaultInteractiveImage(
+                            images: images,
+                            currentIndex:
+                            images.indexOf(widget.element),
+                          )));
+                },
+                child: PhotoView(
+                  disableGestures: true,
+                  imageProvider: AssetImage(widget.element.localUrl),
+                  initialScale: PhotoViewComputedScale.covered,
+                  errorBuilder: (context, error, stackTrace) =>
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            images[images.indexOf(widget.element)]
+                                .localUrl = 'assets/images/a.png';
+                            setState(() {});
+                          },
+                          style: ElevatedButton.styleFrom(
+                              shape: CircleBorder(),
+                              primary: Colors.white.withOpacity(0.5),
+                              minimumSize: Size(50, 50)),
+                          child: const Icon(
+                            Icons.refresh,
+                            color: Colors.black,
                           ),
-                        );
-                      }),
-                    )),
-          )
+                        ),
+                      ),
+                  loadingBuilder: (context, event) => Center(
+                    child: SizedBox(
+                      width: 20.0,
+                      height: 20.0,
+                      child: CircularProgressIndicator(
+                        value: event == null
+                            ? 0
+                            : event.cumulativeBytesLoaded /
+                            event.expectedTotalBytes!,
+                      ),
+                    ),
+                  ),
+                ),
+              ))
         else
           Container(),
       ],
@@ -229,107 +217,99 @@ class _DefaultInteractiveImageState extends State<DefaultInteractiveImage> {
     return Scaffold(
       body: Container(
         child: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: Stack(
-                  children: [
-                    PhotoViewGallery.builder(
-                      pageController: PageController(
-                        initialPage: widget.currentIndex,
-                      ),
-                      loadingBuilder: (context, event) => Center(
-                        child: SizedBox(
-                          width: 20.0,
-                          height: 20.0,
-                          child: CircularProgressIndicator(
-                            value: event == null
-                                ? 0
-                                : event.cumulativeBytesLoaded /
+          child: Container(
+            color: Colors.black,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      PageView(
+                        scrollDirection: Axis.horizontal,
+                        children: widget.images.map((e) => PhotoView(
+                          imageProvider: AssetImage(e.localUrl),
+                          initialScale: PhotoViewComputedScale.contained,
+                          errorBuilder: (context, error, stackTrace) =>
+                              Center(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    images[images.indexOf(e)]
+                                        .localUrl = 'assets/images/a.png';
+                                    setState(() {});
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      primary: Colors.white.withOpacity(0.5),
+                                      minimumSize: Size(50, 50)),
+                                  child: const Icon(
+                                    Icons.refresh,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                          loadingBuilder: (context, event) => Center(
+                            child: SizedBox(
+                              width: 20.0,
+                              height: 20.0,
+                              child: CircularProgressIndicator(
+                                value: event == null
+                                    ? 0
+                                    : event.cumulativeBytesLoaded /
                                     event.expectedTotalBytes!,
-                          ),
-                        ),
-                      ),
-                      builder: (context, index) => PhotoViewGalleryPageOptions(
-                        errorBuilder: (context, error, stackTrace) => Center(
-                          child: ElevatedButton(
-                            onPressed: () {
-                              images[images.indexOf(widget.images[index])]
-                                  .localUrl = 'assets/images/a.png';
-                              setState(() {});
-                            },
-                            style: ElevatedButton.styleFrom(
-                                shape: CircleBorder(),
-                                primary: Colors.white.withOpacity(0.5),
-                                minimumSize: Size(50, 50)),
-                            child: const Icon(
-                              Icons.refresh,
-                              color: Colors.black,
+                              ),
                             ),
                           ),
-                        ),
-                        imageProvider:
-                            AssetImage(widget.images[index].localUrl),
-                        onTapUp: (context, details, controllerValue) {
-                          _isVisible = !_isVisible;
-                          setState(() {});
-                        },
-                        onScaleEnd: (context, details, controllerValue) {
-                          _isVisible = !_isVisible;
-                          setState(() {});
-                        },
-                        minScale: PhotoViewComputedScale.contained * 1,
-                        maxScale: PhotoViewComputedScale.covered * 2.0,
-                        heroAttributes: PhotoViewHeroAttributes(
-                            tag: widget.images.indexOf(widget.images[index])),
+                          onTapDown: (context, details, controllerValue) {
+                            _isVisible=!_isVisible;
+                            setState(() {});
+                          },
+                          minScale: PhotoViewComputedScale.contained * 1,
+                          maxScale: PhotoViewComputedScale.covered * 2.0,
+                        ),).toList(),
+                        allowImplicitScrolling: true,
                       ),
-                      onPageChanged: (index) {
-                        this.index = index;
-                        setState(() {});
-                      },
-                      itemCount: widget.images.length,
-                    ),
-                    AnimatedContainer(
-                      height: _isVisible ? 56.0 : 0.0,
-                      curve: Curves.linear,
-                      duration: Duration(milliseconds: 350),
-                      child: AppBar(
-                        backgroundColor: Colors.black54,
-                        title: Text(
-                          "${index + 1} of ${widget.images.length}",
-                        ),
-                        centerTitle: true,
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: AnimatedContainer(
+                      AnimatedContainer(
                         height: _isVisible ? 56.0 : 0.0,
                         curve: Curves.linear,
                         duration: Duration(milliseconds: 350),
-                        color: Colors.black54,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            IconButton(
-                              icon: Icon(
-                                Icons.download,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.share, color: Colors.white),
-                              onPressed: () {},
-                            )
-                          ],
+                        child: AppBar(
+                          backgroundColor: Colors.black54,
+                          title: Text(
+                            "${index + 1} of ${widget.images.length}",
+                          ),
+                          centerTitle: true,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: AnimatedContainer(
+                          height: _isVisible ? 56.0 : 0.0,
+                          curve: Curves.linear,
+                          duration: Duration(milliseconds: 350),
+                          color: Colors.black54,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                icon: Icon(
+                                  Icons.download,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.share, color: Colors.white),
+                                onPressed: () {},
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
